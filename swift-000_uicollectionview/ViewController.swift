1540e100d5b93583ad6359bb041ce44a4dbc3225
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-11 12:05:33 -0300

    Initial Commit

---
+++ swift-000_uicollectionview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-000_uicollectionview
+//
+//  Created by Luiz Carlos da Silva Araujo on 11/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
