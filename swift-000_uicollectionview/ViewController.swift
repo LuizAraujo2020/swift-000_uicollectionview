51abf658e49ee75d6074a2786b1eacc186edfee8
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-11 12:52:51 -0300

    :gift: Size of the cells

--- swift-000_uicollectionview/ViewController.swift
+++ swift-000_uicollectionview/ViewController.swift
@@ -7,15 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.green
         
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "id")
+        
     }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1    //qtde of sessoes
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 
-
+        return 30
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "id", for: indexPath)
+        
+        cell.backgroundColor = UIColor.yellow
+        
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 80)
+    }
 }
 
