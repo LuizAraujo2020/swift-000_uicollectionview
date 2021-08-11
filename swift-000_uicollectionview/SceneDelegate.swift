143f55295bf9f4cc4773cc39e6a3bc708b2e1e11
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-11 12:38:31 -0300

    :beetle: Fixed the Storyboard bug

--- swift-000_uicollectionview/SceneDelegate.swift
+++ swift-000_uicollectionview/SceneDelegate.swift
@@ -16,7 +16,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        /*
+         BUG of the main.storyboard conflict:
+         https://stackoverflow.com/questions/19240054/could-not-find-a-storyboard-named-main-in-bundle
+         
+         */
+        
+        // Instantiate a ViewController to the root class
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        
+        let mainVC = ViewController()
+        
+        window?.windowScene = windowScene
+        window?.rootViewController = mainVC
+        window?.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
